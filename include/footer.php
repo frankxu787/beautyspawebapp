c2deac59587412f535ea8a8ef8a667543439276b
frankxu787 <93010996+frankxu787@users.noreply.github.com>
2022-10-21 16:35:22 -0700

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,9 @@
+
+
+
+    <div class="container-footer">
+      <p class="copyright">Created by: Frank Xu - NU CSC680 Final Project - Oct 2022.</p>
+    </div>
+
+  </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CSC680 Final Project</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" href="image/favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans&family=Montserrat&family=Sacramento&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container-header">
+      <table class="headertable">
+        <tr>
+          <th>
+            <a href="home.php#go-to-home">Home</a>
+          </th>
+          <th>
+            <a href="home.php#go-to-about">About</a>
+          </th>
+          <th>
+            <a href="home.php#go-to-menu">Menu</a>
+          </th>
+          <th>
+            <a href="home.php#go-to-appointment">Appointment</a>
+          </th>
+          <th>
+            <a href="home.php#go-to-contact">Contact Us</a>
+          </th>
+        </tr>
+      </table>
+    </div>
